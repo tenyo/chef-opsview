f69ee4612104f0369899278a4455441e773872f9
Jared King <jared.king@biola.edu>
2015-01-13 12:27:56 -0800

    Metadata update

--- metadata.rb
+++ metadata.rb
@@ -8,9 +8,13 @@ version          '3.0.0'
 depends          'apt', '~> 2.5.0'
 depends          'ark', '~> 0.9'
 depends          'chef-vault', '>= 1.0'
-depends          'mysql', '< 6'
+depends          'mysql', '< 6.0'
 depends          'openssl', '~> 2.0.2'
 depends          'oracle_instant_client', '~> 0.2.0'
 depends          'vsphere_perl_sdk', '~> 1.0.0'
 depends          'yum', '~> 3.2.4'
 depends          'yum-epel'
+
+%w(ubuntu redhat centos windows).each do |os|
+  supports os
+end
